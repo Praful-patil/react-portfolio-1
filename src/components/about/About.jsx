df72363db931482dee079488098ce198a6016009
Praful <prafulpatil276@gmail.com>
2022-09-20 18:57:07 +0530

    About Update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -31,7 +31,7 @@ const About = () => {
             <article className='about__card'>
               <VscFolderLibrary className='about__icon' />
               <h5>Projects</h5>
-              <small>20+ Completed</small>
+              <small>10+ Completed</small>
             </article>
             </div>
           
