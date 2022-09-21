8d8dd7730c104615686139f86a0f206c1ebf7b50
Praful <prafulpatil276@gmail.com>
2022-09-21 16:13:31 +0530

    Changes

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
+
+
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Tina Snow',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis hic ad laboriosam minus eius nobis impedit provident quas libero accusamus veritatis dolor voluptatem, modi consequuntur corrupti sit qui rem labore!'
+  }
+]
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonial</h2>
+
+      <div className="container testimonials__container">
+        {
+          data.map(({ avatar, name, review }, index) => {
+            return (
+              <article key={index} className="testimonial">
+                <div className="client__avatar">
+                  <img src={avatar} />
+                </div>
+                <h5 className='client__name'>{name}</h5>
+                <small className='client__review'>{review} </small>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
