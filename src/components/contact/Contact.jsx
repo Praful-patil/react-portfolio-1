60d18f5fb0a255a01dd9d3538a8e4d3c4d2718ee
Praful <prafulpatil276@gmail.com>
2022-09-22 12:23:29 +0530

    final commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineMail} from 'react-icons/md'
+import {FaFacebookMessenger} from 'react-icons/fa'
+import {BsWhatsapp} from 'react-icons/bs'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section >
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+    
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className='contact__option'>
+            <MdOutlineMail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>prafulpatil276@gmail.com</h5>
+            <a href="mailto:prafulpatil276@gmail.com">Send a Message</a>
+          </article>
+          <article className='contact__option'>
+            <FaFacebookMessenger className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>Blizzard</h5>
+            <a href="mailto:prafulpatil276@gmail.com">Send a Message</a>
+          </article>
+          <article className='contact__option'>
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>Whatsapp</h4>
+            <h5>+1234567890</h5>
+            <a href="mailto:prafulpatil276@gmail.com">Send a Message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required/>
+          <input type="email" name="email" placeholder='Your Email' required />
+          <textarea name="message" placeholder="Your Message" rows="7" required></textarea>
+          <button type='submit' className='btn btn-primary' >Send Message</button>
+        </form>
+      </div>
+    </section >
   )
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import './footer.css'
+import {GrFacebookOption} from 'react-icons/gr'
+import {FiInstagram} from 'react-icons/fi'
+import {IoLogoTwitter} from 'react-icons/io'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Praful</a>
+
+      <ul className='permalinks'>
+        <li><a href="">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Experience</a></li>
+        <li><a href="">Services</a></li>
+        <li><a href="">Portfolio</a></li>
+        <li><a href="">Testimonial</a></li>
+        <li><a href="">Contact</a></li>
+      </ul>
+
+
+      <div className="footer__socials">
+        <a href="https://facebook.com"><GrFacebookOption/></a>
+        <a href="https://instagram.com"><FiInstagram/></a>
+        <a href="https://twitter.com"><IoLogoTwitter/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; EGATOR Tutorials. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
